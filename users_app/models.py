fecbafabcb3adf9ce5be4b961ad384938c191c36
Harshal Marathe <dev.harshalmarathe@gmail.com>
2025-05-26 00:48:30 +0530

    feature: create and migrate profile-db

--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    display_name = models.CharField(max_length=100, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
+    
+
