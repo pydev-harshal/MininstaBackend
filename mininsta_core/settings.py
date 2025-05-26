48376d6025d33278f9d6d1656c1c5fc13ae04b3d
Harshal Marathe <dev.harshalmarathe@gmail.com>
2025-05-27 01:08:48 +0530

    feature: add global pagination to settings

--- mininsta_core/settings.py
+++ mininsta_core/settings.py
@@ -81,7 +81,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
-    'SEARCH_PARAM': 'q'
+    'SEARCH_PARAM': 'q',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 3
 }
 
 SIMPLE_JWT = {
